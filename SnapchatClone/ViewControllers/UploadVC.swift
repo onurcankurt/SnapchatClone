6d731f3cec88af886c2b65021c83886b201af312
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-18 16:25:05 +0300

    upload process in uploadvc

--- SnapchatClone/ViewControllers/UploadVC.swift
+++ SnapchatClone/ViewControllers/UploadVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseFirestoreInternal
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -13,7 +15,7 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         uploadImageView.isUserInteractionEnabled = true;
@@ -34,8 +36,54 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     }
     
     @IBAction func uploadClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = uploadImageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            
+            imageReference.putData(data) { metadata, error in
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                } else {
+                    
+                    imageReference.downloadURL { url, error in
+                        if error == nil {
+                            
+                            let imageUrl = url?.absoluteString
+                            
+                            let fireStore = Firestore.firestore()
+                            
+                            let snapDictionary = ["imageUrl" : imageUrl!, "snapOwner" : UserSingleton.sharedUserInfo.username, "date" : FieldValue.serverTimestamp()] as [String : Any]
+                            
+                            fireStore.collection("Snaps").addDocument(data: snapDictionary) { error in
+                                if error != nil {
+                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                                    
+                                } else {
+                                    self.tabBarController?.selectedIndex = 0
+                                    self.uploadImageView.image = UIImage(named: "selectImage")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
     
 }
